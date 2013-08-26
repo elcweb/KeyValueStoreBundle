59cfb17d5cd29cb54f221ac0796507187d4c13a9
Ziad Nahas <ziad.nahas@gmail.com>
2013-08-26 14:05:52 -0400

    ELEN-19 - Modified the KeyValueStore to handle wildcards

--- KeyValueStore.php
+++ KeyValueStore.php
@@ -14,6 +14,12 @@ class KeyValueStore
         $this->em = $em;
     }
 
+    /**
+     * Gets the value of the exact key
+     *
+     * @param string $key
+     * @return mixed
+     */
     public function get($key)
     {
         $value = $this->em->getRepository('ElcwebKeyValueStoreBundle:KeyValue')->findOneByKey($key);
@@ -25,7 +31,14 @@ class KeyValueStore
         return $value->getValue();
     }
 
-    public function set($key, $value, $description = "")
+    /**
+     * Adds a new pair of key value
+     *
+     * @param mixed $key
+     * @param mixed $value
+     * @param string $description
+     */
+    public function set($key, $value, $description = '')
     {
         $keyvalue = $this->em->getRepository('ElcwebKeyValueStoreBundle:KeyValue')->findOneByKey($key);
 
@@ -39,5 +52,31 @@ class KeyValueStore
 
         $this->em->persist($keyvalue);
         $this->em->flush();
+    }
+
+    /**
+     * Gets all the matching key prefixs Ex. '$key%' and returns array of postfix keys
+     *
+     * @param $key
+     * @return array
+     */
+    public function getAll($key)
+    {
+        $qb = $this->em->createQueryBuilder();
+
+        $result = $qb->select('u')
+            ->from('ElcwebKeyValueStoreBundle:KeyValue', 'u')
+            ->where($qb->expr()->like('u.key', ':key'))
+            ->setParameter('key', "{$key}%")
+            ->getQuery()
+            ->getResult();
+
+        $return = array();
+
+        foreach ($result as $row) {
+            $return[substr($row->getKey(), strlen($key))] = $row->getValue();
+        }
+
+        return $return;
     }
 }
