3be4ce3f9b5cf3fde9cf8739aa5c52cba032e600
Etienne Lachance <et@etiennelachance.com>
2014-01-15 14:01:27 -0500

    Add EventListener

---
+++ EventListener/KeyValueListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Elcweb\KeyValueStoreBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Doctrine\ORM\EntityManager;
+use Elcweb\KeyValueStoreBundle\KeyValueStore;
+
+class KeyValueCreateListener
+{
+
+    public function __construct(KeyValueStore $kv)
+    {
+        $this->kv = $kv;
+    }
+
+
+    public function onEvent(GenericEvent $event)
+    {
+        $model = $event->getArgument('keyValue');
+
+        $this->kv->set($model->getKey(), $model->getValue(), $model->getDescription());
+    }
+}
